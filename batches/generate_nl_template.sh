937f9917d53cbe0f3dcab257f6216af332191301
Fazle Rabbi <frduiit@gmail.com>
2025-12-25 00:25:43 -0500

    Update generate_nl_template.sh

--- batches/generate_nl_template.sh
+++ batches/generate_nl_template.sh
@@ -16,6 +16,6 @@ module load cuda/12.3.2
 module load anaconda/3.2024.10.1
 
 eval "$(conda shell.bash hook)"
-conda activate nlspec
+conda activate code_trans
 cd ../
 python nlspec_adapter.py magicoder /home/f_rabbi/models ##DATASET## ##LANG##
