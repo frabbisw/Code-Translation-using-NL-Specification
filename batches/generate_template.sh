61dbd2c6a2c61447ff90c08a02331241494a0f43
Fazle Rabbi <frduiit@gmail.com>
2026-01-01 11:02:18 -0500

    Update generate_template.sh

--- batches/generate_template.sh
+++ batches/generate_template.sh
@@ -8,12 +8,18 @@
 #SBATCH -o _%x%J.out
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=osdefr@gmail.com
+########
 
 source /etc/profile.d/modules.sh
 
+module load java/17.0.2
+module load gcc/11.5
+module load go/1.24.5
 module load python/3.11.6
 module load cuda/12.3.2
 module load anaconda/3.2024.10.1
+
+export PATH="$HOME/my-nodejs/bin:$PATH"
 
 eval "$(conda shell.bash hook)"
 conda activate code_trans
