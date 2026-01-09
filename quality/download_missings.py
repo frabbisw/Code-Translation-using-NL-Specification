6de3abf16dd9d863936f661c6b3232096a8097f0
Fazle Rabbi <frduiit@gmail.com>
2026-01-09 04:16:30 -0500

    Update download_missings.py

--- quality/download_missings.py
+++ quality/download_missings.py
@@ -5,15 +5,22 @@ import sys
 root = sys.argv[1]
 org = sys.argv[2] 
 
-files = [f for f in os.listdir(root) if f.startswith("details") and f.endswith(".json")]
-for file in files:
-    with open(f"{root}/{file}", "r") as f:
-        code = f.read()
-        if len(code) < 5:
-            print(root, file)
-            _, dataset, src, target, __ = file.split("_")
-            result = subprocess.run(["python", "get_info.py", "G", dataset, src, target, root, org], capture_output=True, text=True)
-            if result.returncode == 0:
-                print(f"Command executed successfully. Output:\n{result.stdout}")
-            else:
-                print(f"Command failed with error:\n{result.stderr}")
+src_dir = sys.argv[1]
+tgt_path = sys.argv[2]
+organization = sys.argv[3]
+project_key = sys.argv[4]
+
+print(f"checking missing file {tgt_path}")
+
+with open(f"{tgt_path}_details.json", "r") as f:
+    code = f.read()
+if len(code) < 5:
+    print(f"missing: {tgt_path}")
+    print("fetching again ...")
+    result = subprocess.run(["python", "get_info.py", src_dir, tgt_path, organization, project_key], capture_output=True, text=True)
+    if result.returncode == 0:
+        print(f"Command executed successfully. Output:\n{result.stdout}")
+    else:
+        print(f"Command failed with error:\n{result.stderr}")
+else:
+    print("file is okay!")
