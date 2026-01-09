3d12f0fbce53cbb5f07a5d8b31a382e01cfa3ba2
Fazle Rabbi <frduiit@gmail.com>
2026-01-09 03:42:53 -0500

    Create create_batches.sh

---
+++ quality/create_batches.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+TEMPLATE_FILE="template.sh"
+MODEL="magicoder"
+ORG_NAME="nlspec"
+
+datasets=("avatar" "codenet" "codenetintertrans")
+
+# Dataset-specific language lists
+source_languages_avatar=("Java" "Python")
+target_languages_avatar=("C++" "C" "Java" "Go" "Python")
+
+source_languages_codenet=("C" "C++" "Go" "Python" "Java")
+target_languages_codenet=("C" "C++" "Go" "Python" "Java")
+
+source_languages_codenetintertrans=("C++" "Go" "Python" "Java" "Javascript" "Rust")
+target_languages_codenetintertrans=("C++" "Go" "Python" "Java" "Javascript" "Rust")
+
+for dataset in "${datasets[@]}"; do
+    case "$dataset" in
+        avatar)
+            source_languages=("${source_languages_avatar[@]}")
+            target_languages=("${target_languages_avatar[@]}")
+            ;;
+        codenet)
+            source_languages=("${source_languages_codenet[@]}")
+            target_languages=("${target_languages_codenet[@]}")
+            ;;
+        codenetintertrans)
+            source_languages=("${source_languages_codenetintertrans[@]}")
+            target_languages=("${target_languages_codenetintertrans[@]}")
+            ;;
+        *)
+            echo "Unknown dataset: $dataset"
+            exit 1
+            ;;
+    esac
+
+    for src_lang in "${source_languages[@]}"; do
+        for tgt_lang in "${target_languages[@]}"; do
+            if [[ "$src_lang" == "$tgt_lang" ]]; then
+                continue
+            fi
+
+            output_file="generate_${dataset}_${src_lang}_${tgt_lang}.sh"
+
+            # Random node selection
+            X=$((RANDOM % 4 + 1))
+            sed_extra=(-e "s/########/#SBATCH -w virya${X}/g")
+
+            sed \
+                -e "s/##DATASET##/${dataset}/g" \
+                -e "s/##SRC_LANG##/${src_lang}/g" \
+                -e "s/##TGT_LANG##/${tgt_lang}/g" \
+                -e "s/##MODEL##/${MODEL}/g" \
+                -e "s/##ORG_NAME##/${ORG_NAME}/g" \
+                "${sed_extra[@]}" \
+                "$TEMPLATE_FILE" > "$output_file"
+
+            chmod +x "$output_file"
+        done
+    done
+done
