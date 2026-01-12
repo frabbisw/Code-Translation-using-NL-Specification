6e70f80c76825cb59aab94d9a71a829643ac9ae1
Fazle Rabbi <frduiit@gmail.com>
2026-01-12 00:48:09 -0500

    Update template.sh

--- batches/template.sh
+++ batches/template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -J starcoder_##DATASET##_##SRC_LANG##_##TGT_LANG##
+#SBATCH -J ##MODEL##_##DATASET##_##SRC_LANG##_##TGT_LANG##
 #SBATCH -n1
 #SBATCH --mem=60GB
 #SBATCH --gpus=2
