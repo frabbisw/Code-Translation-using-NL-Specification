14eab54abeded8c2f3650bdd9d65a33914ce3cc5
Fazle Rabbi <frduiit@gmail.com>
2026-01-16 20:12:39 -0500

    Update create_batch_generate_nl.sh

--- batches/create_batch_generate_nl.sh
+++ batches/create_batch_generate_nl.sh
@@ -50,7 +50,7 @@ for dataset in "${datasets[@]}"; do
                 continue
             fi
 
-            output_file="generate_${dataset}_${src_lang}_${tgt_lang}.sh"
+            output_file="generate_${MODEL}_${dataset}_${src_lang}_${tgt_lang}.sh"
 
             # Random node selection
             X=$((RANDOM % 4 + 1))
