435326f5dda7f01b93724c8cd2c64b65ebc66d5d
Fazle Rabbi <frduiit@gmail.com>
2026-01-22 01:37:29 -0500

    Update template.sh

--- batches/template.sh
+++ batches/template.sh
@@ -16,7 +16,7 @@ module load java/17.0.2
 module load gcc/11.5
 module load go/1.24.5
 module load python/3.11.6
-module load cuda/12.3.2
+module load cuda/12.6.3
 module load anaconda/3.2024.10.1
 
 export PATH="$HOME/my-nodejs/bin:$PATH"
